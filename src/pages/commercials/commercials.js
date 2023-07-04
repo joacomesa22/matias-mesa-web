a1866bebc721547ad07b783c997db9f5b5c7940d
joacomesa22 <joacomesa@gmail.com>
2023-07-04 11:01:43 -0300

    adding movies and comms

---
+++ src/pages/commercials/commercials.js
@@ -0,0 +1,73 @@
+export const commercials = [
+  {
+    title: "Wohninvest 'End of Decadence'",
+    prod: "Bonaparte Films Berlin",
+    dir: "Nico Kreis",
+  },
+  {
+    title: "Momondo",
+    prod: "Lomo",
+    dir: "Sebastian Blenkov",
+  },
+  {
+    title: "Pepsi black... So what",
+    prod: "Primo",
+    dir: "Nico Perez Veiga",
+  },
+  {
+    title: "VW Duality",
+    prod: "Primo",
+    dir: "Nico Perez veiga",
+  },
+  {
+    title: "Revlon",
+    prod: "a WHITELABEL product",
+    dir: "Darius Khondji",
+  },
+  {
+    title: "Target",
+    prod: "a WHITELABEL product",
+    dir: "Darius Khondji",
+  },
+  {
+    title: "Prudential",
+    prod: "Brainwash",
+    dir: "Carlos Veron",
+  },
+  {
+    title: "Ford",
+    prod: "La DobleA",
+    dir: "Esteban Sapir",
+  },
+  {
+    title: "Clarin",
+    prod: "Rebolucion",
+    dir: "Lemon",
+  },
+  {
+    title: "Coke Lift",
+    prod: "Rebolucion",
+    dir: "Armando Bo",
+  },
+  {
+    title: "AFP Popular",
+    prod: "Banana Films",
+    dir: "Maria Jorda",
+  },
+];
+
+//   const commercialsContainer = document.querySelector("#commercials");
+
+//   commercials.forEach((comm) => {
+//     let commCard = document.createElement("div");
+//     commCard.classList.add("portfolio__card");
+//     commCard.innerHTML = `
+//           <div class="portfolio__card-comm">
+//               <h4>${comm.title}</h4>
+//               <p>${comm.prod != "" ? "Prod: " + comm.prod : ""}</p>
+//               <p>${comm.dir != "" ? "Dir: " + comm.dir : ""}</p>
+//           </div>
+
+//       `;
+//     commercialsContainer.appendChild(commCard);
+//   });
---
+++ src/pages/movies/movies.js
@@ -0,0 +1,289 @@
+export const movies = [
+  // {
+  //   title: "Lioness",
+  //   prod: "Paramount +",
+  //   dir: "John Hilcoat, Anthony Byrne",
+  //   dp: "Paul Camerona, Eic Koretz, Nicolas Karakastanis",
+  //   img: "#",
+  //   link: "#",
+  // },
+  // {
+  //   title: "Lady in the Lake",
+  //   prod: "Apple TV+",
+  //   dir: "Alma Ha'rel",
+  //   dp: "Lachlan Milne, M. Mesa",
+  //   img: "#",
+  //   link: "#",
+  // },
+  // {
+  //   title: "Young Woman and the sea",
+  //   prod: "Jerry Bruckheimer Films",
+  //   dir: "Joachim Ronning",
+  //   dp: "Oscar Faura",
+  //   img: "#",
+  //   link: "#",
+  // },
+  {
+    title: "Desert Warrior",
+    prod: "Lightfuse & Gettaway",
+    dir: "Ruphert Wyatt",
+    dp: "Guillermo Garza AMC",
+    img: "../img/desert-warrior.jpg",
+    link: "#",
+  },
+  {
+    title: "Monster, The Jeffrey Dhamer Story",
+    prod: "Ryan Murphy Productions",
+    dir: "Paris Barclay",
+    dp: "Jason McCormick",
+    img: "../img/dahmer.jpeg",
+    link: "https://www.youtube.com/watch?v=4549vhvWFo8",
+  },
+  {
+    title: "Tom Clancy's Jack Ryan S2",
+    prod: "Genre Artist",
+    dir: "Andrew Bernstein",
+    dp: "Arnau Valls Colomer",
+    img: "../img/jack-r.jpg",
+    link: "https://www.youtube.com/watch?v=II06Vwx5Jc4&t=2s",
+  },
+  {
+    title: "Riviera",
+    prod: "Archery Pictures",
+    dir: "Paul Walker",
+    dp: "Philip Kress",
+    img: "../img/rivera.jpg",
+    link: "https://youtu.be/EKP8tC1oSbw",
+  },
+  {
+    title: "Okja",
+    prod: "Netflix",
+    dir: "Bong Joon Ho",
+    dp: "Darius Kondji ASC",
+    img: "../img/okja.jpg",
+    link: "https://www.youtube.com/watch?v=AjCebKn4iic",
+  },
+  {
+    title: "Honey Boy",
+    prod: "Automatik / Amazon Studios",
+    dir: "Alma Har'el",
+    dp: "Natasha Braier",
+    img: "../img/honeyboy.jpg",
+    link: "https://www.youtube.com/watch?v=5RR8WTQzwSk",
+  },
+  {
+    title: "Replicas",
+    prod: "Replicas Holdings",
+    dir: "Jeffrey Nachmanoff",
+    dp: "Checco Varese ASC",
+    img: "../img/replicas.jpg",
+    link: "https://www.youtube.com/watch?v=ciqeJ7SBRdQ",
+  },
+  {
+    title: "Triple Nine",
+    prod: "Worldview Entertainment",
+    dir: "John Hillcoat",
+    dp: "Nick Karakapsanis",
+    img: "../img/triple-9.jpg",
+    link: "https://www.youtube.com/watch?v=vOFqiwmnOYE",
+  },
+  {
+    title: "The Games Maker",
+    prod: "Pampa Films / Walt Disney",
+    dir: "Juan Pablo Buscarini",
+    dp: "Roman Osin",
+    img: "../img/gamesmaker.jpg",
+    link: "https://www.youtube.com/watch?v=F66dsn_NLqo",
+  },
+  {
+    title: "And soon the darkness",
+    prod: "Abandon Pictures",
+    dir: "Marcos Effron",
+    dp: "Gabriel Beristain",
+    img: "../img/darkness.jpg",
+    link: "https://www.youtube.com/watch?v=GO8BZF_yNBA",
+  },
+  {
+    title: "There be dragons",
+    prod: "Independent",
+    dir: "Roland Joffe",
+    dp: "Gabriel Beristain",
+    img: "../img/dragons.jpg",
+    link: "https://www.youtube.com/watch?v=kWVy91GzfsU",
+  },
+  {
+    title: "The Road",
+    prod: "2929 Productions",
+    dir: "John Hillcoat",
+    dp: "Javier Aguirresarobe",
+    img: "../img/the-road.jpg",
+    link: "https://www.youtube.com/watch?v=tloYv8bUAHY",
+  },
+  {
+    title: "The Informers",
+    prod: "Senator Film Produktion",
+    dir: "Gregor Jordan",
+    dp: "Petra Korner",
+    img: "../img/informers.jpg",
+    link: "https://www.youtube.com/watch?v=HxkvJkSIOGk",
+  },
+  {
+    title: "Camp Hell",
+    prod: "Holedigger Films",
+    dir: "George VanBuskirk",
+    dp: "Michael McDonough",
+    img: "../img/camphell.jpg",
+    link: "https://www.youtube.com/watch?v=A5H5KZcCuDw&t=1s",
+  },
+  {
+    title: "The city of your final destination",
+    prod: "Merchant Ivory Productions",
+    dir: "James Ivory",
+    dp: "Javier Aguirresarobe",
+    img: "../img/destination.jpg",
+    link: "https://www.youtube.com/watch?v=ZZSf79c8lcU",
+  },
+  {
+    title: "El Pasado",
+    prod: "HB Films, Oscar Kramer",
+    dir: "Hector Babenco",
+    dp: "Ricardo Dellarosa",
+    img: "../img/pasado.jpg",
+    link: "https://www.youtube.com/watch?v=Aj2qrt1oyos&t=23s",
+  },
+  {
+    title: "Babel",
+    prod: "Paramount Vantage",
+    dir: "Alejandro G. Inarritu",
+    dp: "Rodrigo Prieto",
+    img: "../img/babel.jpg",
+    link: "https://www.youtube.com/watch?v=yDNa6t-TDrQ",
+  },
+  {
+    title: "Last Days",
+    prod: "HBO Films, Fine Line Feature",
+    dir: "Gus Van Sant",
+    dp: "Harris Savides",
+    img: "../img/lastdays.jpg",
+    link: "https://www.youtube.com/watch?v=HFWnZW3esb8",
+  },
+  {
+    title: "El Aura",
+    prod: "Patagonik Films, Tornasol",
+    dir: "Fabian Bielinsky",
+    dp: "Franchesco Varesse",
+    img: "../img/aura.jpeg",
+    link: "https://www.youtube.com/watch?v=aJpAJCBHwjY",
+  },
+  {
+    title: "El Custodio",
+    prod: "Rizoma Films, Primer Plano",
+    dir: "Rodrigo Moreno",
+    dp: "Barbara Alvarez",
+    img: "../img/custodio.jpg",
+    link: "https://www.youtube.com/watch?v=UKYLsserHMw",
+  },
+  {
+    title: "Blessed by Fire",
+    prod: "UN SAM, Canal+, IN CAA",
+    dir: "Tristan Bauer",
+    dp: "Javier Julia",
+    img: "../img/blessedbyfire.jpg",
+    link: "https://www.youtube.com/watch?v=c3Pyou7P4DU",
+  },
+  {
+    title: "Elephant",
+    prod: "Fine Line Features",
+    dir: "Gus Van Sant",
+    dp: "Harris Savides",
+    img: "../img/elephant.jpg",
+    link: "https://www.youtube.com/watch?v=htlsOf3PnGY",
+  },
+  {
+    title: "Water",
+    prod: "Bambu, Archipel 35",
+    dir: "Veronica Chen",
+    dp: "Sabine Lancelin",
+    img: "../img/agua.jpg",
+    link: "https://www.youtube.com/watch?v=Ln0wI51hpZI",
+  },
+  {
+    title: "Whiskey, Romeo, Zulu",
+    prod: "Aquafilms",
+    dir: "Enrique Pineyro",
+    dp: "Ramiro Chivita",
+    img: "../img/whisky.jpg",
+    link: "https://www.youtube.com/watch?v=BZWm5ghcrxM",
+  },
+  {
+    title: "The Whore & The Whale",
+    prod: "Patagonik, Wanda Vision",
+    dir: "Luis Puenzo",
+    dp: "Jose Luis Alcaine",
+    img: "../img/whorewhale.jpg",
+    link: "https://www.youtube.com/watch?v=tW9obud1YOA",
+  },
+  {
+    title: "Gerry",
+    prod: "Miramax",
+    dir: "Gus Van Sant",
+    dp: "Harris Savides",
+    img: "../img/gerry.jpg",
+    link: "https://www.youtube.com/watch?v=5LHKhsXvjMo",
+  },
+  {
+    title: "Nine Queens",
+    prod: "Patagonik, Sony Pictures",
+    dir: "Fabian Bielinsky",
+    dp: "Marcelo Camorino",
+    img: "../img/nueve-reinas.jpg",
+    link: "https://www.youtube.com/watch?v=P63PyjeDoKk",
+  },
+  {
+    title: "The walking dead - S10",
+    prod: "AMC",
+    dir: "Greg Nikotero",
+    dp: "Duane Manwiller",
+    img: "../img/twd.jpg",
+    link: "https://www.youtube.com/watch?v=DB8x81nL8B0",
+  },
+  {
+    title: "Operation Finale",
+    prod: "MGM",
+    dir: "Chris Weitz",
+    dp: "Javier Aguirresarobe",
+    img: "../img/operacion-f.jpg",
+    link: "https://www.youtube.com/watch?v=07Y4_6PD0Z4",
+  },
+  {
+    title: "Beyond Sleep",
+    prod: "Key Films",
+    dir: "Boudewijn Koole",
+    dp: "Melle van Essen",
+    img: "../img/beyond-sleep.jpg",
+    link: "https://www.youtube.com/watch?v=TtmYZ-mjEX0",
+  },
+];
+
+//   const moviesContainer = document.querySelector("#movies");
+
+//   movies.forEach((mov) => {
+//     let movCard = document.createElement("div");
+//     movCard.classList.add("portfolio__card");
+//     movCard.innerHTML = `
+//         <div class="portfolio__card">
+//             <a href="${mov.link}" target="_blank">
+//             <div class="portfolio__card-img">
+//                 <img src="${mov.img}" alt="${mov.title.toLowerCase()}" />
+//             </div>
+//             <div class="portfolio__card-text">
+//                 <h4>${mov.title}</h4>
+//                 <p>${mov.prod != "" ? "Prod: " + mov.prod : ""}</p>
+//                 <p>${mov.dir != "" ? "Dir: " + mov.dir : ""}</p>
+//                 <p>${mov.dp != "" ? "DP: " + mov.dp : ""}</p>
+//             </div>
+//             </a>
+//         </div>
+//         `;
+//     moviesContainer.appendChild(movCard);
+//   });
